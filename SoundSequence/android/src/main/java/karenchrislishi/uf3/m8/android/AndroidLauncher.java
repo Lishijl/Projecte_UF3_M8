0841960f5c9ee70720da0421314e568ee343d628
Lishi Ji Liu <liulishij@gmail.com>
2025-04-25 16:59:45 +0200

    SoundSequence init

---
+++ SoundSequence/android/src/main/java/karenchrislishi/uf3/m8/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package karenchrislishi.uf3.m8.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import karenchrislishi.uf3.m8.SoundSequence;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new SoundSequence(), configuration);
+    }
+}
